4ca2be892203b4ac8b7b513c14a48913b4897c2b
ElWampoStompo <167680659+ElWampoStompo@users.noreply.github.com>
2024-07-29 23:07:00 +1000

    lualine status codeium intergration

--- lua/wampa/plugins/lualine.lua
+++ lua/wampa/plugins/lualine.lua
@@ -4,6 +4,26 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("lualine").setup()
+        require("lualine").setup({
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 
+                    {
+                        'diagnostics',
+                        sources = { 'nvim_lsp' },
+                    },
+                    {
+                        'ai Status',
+                        function()
+                            return vim.api.nvim_call_function("codeium#GetStatusString", {})
+                        end
+                    }
+                },
+            },
+        })
     end,
 }
