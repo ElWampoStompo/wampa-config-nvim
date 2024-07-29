bc2d4d5da21bed188b97cc058ff5981ce8c22b68
ElWampoStompo <167680659+ElWampoStompo@users.noreply.github.com>
2024-07-29 23:06:25 +1000

    expand codeium config

--- lua/wampa/plugins/codeium.lua
+++ lua/wampa/plugins/codeium.lua
@@ -2,6 +2,17 @@
 
 return {
     'Exafunction/codeium.vim',
-    event = 'BufEnter',
+    -- event = 'BufEnter',
+    event = 'InsertEnter', -- Load plugin on entering insert mode
+    config = function() -- Your config
+        vim.g.codeium_disable_bindings = 1
+        -- Accept : codeium#Accept() - Accept the current completion
+        vim.api.nvim_set_keymap('i', '<C-g>', 'codeium#Accept()', { expr = true, silent = true })
+        -- Cycle through completions with ; and ,
+        vim.api.nvim_set_keymap('i', '<C-;>', 'codeium#CycleCompletions(1)', { expr = true, silent = true })
+        vim.api.nvim_set_keymap('i', '<C-,>', 'codeium#CycleCompletions(-1)', { expr = true, silent = true })
+        -- Clear current codeium
+        vim.api.nvim_set_keymap('i', '<C-x>', 'codeium#Clear()', { expr = true, silent = true })
+    end
 }
 
